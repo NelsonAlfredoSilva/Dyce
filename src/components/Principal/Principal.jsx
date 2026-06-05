44e3bdf253ee6ec5645f008d0a6a03bb187240eb
nelsonalfredosilva@gmail.com <nelsonalfredosilva@gmail.com>
2026-06-05 14:37:23 -0300

    Modificaciones de los css y agregado de algunos componentes

--- src/components/Principal/Principal.jsx
+++ src/components/Principal/Principal.jsx
@@ -1,10 +1,11 @@
+import { Cards } from '../Cards/Cards'
 import './Principal.css'
 export const Principal = () => {
     return (
-        <div className="contenedor">
-            <section className='size_50 direccion_en_columna'>
+        <div className="contenedor__principal">
+            <section className='size_50per direccion_en_columna'>
                 <div className='resaltado_verde'>Componentes Electrónicos de Alta Calidad</div>
-                <p className='size_50 titular'>
+                <p className='size_50per titular'>
                     La mejor <span className='fuente_azul'>Tecnología</span> al
                     Alcance de tus 
                     Proyectos
@@ -13,32 +14,32 @@ export const Principal = () => {
                     <p>Mas de 10.000 Componentes electrónicos en stock.</p>
                     <p>Distribuidores Autorizados de las mejores marcas mundiales.</p>
                 </div>
-                <div className='boton_opciones'>
+                <div className='commandGroup'>
                     <button>Ver Catálogo</button>
                     <button>Contactar Ventas</button>
                 </div>
             </section>
             
-            <section className='size_50'>
-                <div>
-                    <div>
+            <section className='size_50per'>
+                <div className='size_50per direccion_en_columna justificado_con_espacios'>
+                    <Cards>
                         <p>Semiconductores</p>
                         <p>+3.000 referencias</p>
-                    </div>
-                    <div>
+                    </Cards>
+                    <Cards>
                         <h1>10K+</h1>
                         <p>Productos en Stock</p>
-                    </div>
+                    </Cards>
                 </div>
                 <div>
-                    <div>
+                    <Cards>
                         <p>Envío en 24hs</p>
                         <p>a todo el pais</p>
-                    </div>
-                    <div className='fondo_azul'>
+                    </Cards>
+                    <Cards estiloAdd = "colorFondo_azul">
                         <h1>15+</h1>
                         <h5>Años de experiencia</h5>
-                    </div>
+                    </Cards>
                 </div>
             </section>
         </div>
