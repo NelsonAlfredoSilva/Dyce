dbd275b6711f010fcb09f7263eab3a01de109723
Leia Silva <leiabelensilva@gmail.com>
2026-08-13 19:24:16 -0300

    agregue texto en quienes somos

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,16 +5,21 @@ import { MdAlternateEmail } from "react-icons/md"
 import { FaPhone, FaWhatsapp } from "react-icons/fa"
 import { SiGooglemaps } from "react-icons/si"
 import { MapsLink } from "../MapsLink/MapsLink"
+import waveFooter from "../../assets/waveFooter.svg";
 export const Footer = () => {
     return (
         <footer>
-            <div>
-                <img src={logo} alt="Logotipo Dyce S.A." className="imagen_logo"/>
+
+
+
+            
+            <div className="footerContainer">
+                <img src={logo} alt="Logotipo Dyce S.A." className="imagen_logo" />
                 <p className="titulo_footer">Dyce S.A.</p>
                 <p>Proveedor Lider en ventas al gremio de componentes electrónicos de alta calidad
-                 para la industria y el desarrollo tecnológico.</p>
+                    para la industria y el desarrollo tecnológico.</p>
             </div>
-            <div>
+            <div className="footerContainer">
                 <h3>ENLACES</h3>
                 <ul
                     className="ul_footer"
@@ -58,13 +63,14 @@ export const Footer = () => {
             <div>
                 <h3>CONTACTO</h3>
                 <ul className="ul_footer">
-                    <li><MdAlternateEmail/> <a href="mailto:info@dyce.com.ar"> info@dyce.com.ar</a></li>
-                    <li><FaPhone/> +54 11 5486 9502</li>
-                    <li><FaWhatsapp/> +54 11 4931-0069</li>
-                    <li><SiGooglemaps/>
-                    <a href="https://maps.app.goo.gl/dZAQV5zCt3ui8BYw7" target="_blank" rel="noopener noreferrer">México 4067, C1223 Cdad. Autónoma de Buenos Aires</a></li>
+                    <li><MdAlternateEmail /> <a href="mailto:info@dyce.com.ar"> info@dyce.com.ar</a></li>
+                    <li><FaPhone /> +54 11 5486 9502</li>
+                    <li><FaWhatsapp /> +54 11 4931-0069</li>
+                    <li><SiGooglemaps />
+                        <a href="https://maps.app.goo.gl/dZAQV5zCt3ui8BYw7" target="_blank" rel="noopener noreferrer">México 4067, C1223 Cdad. Autónoma de Buenos Aires</a></li>
                 </ul>
             </div>
         </footer>
     )
 }
+/*<img src={waveFooter} alt="" />*/
